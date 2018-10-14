1d5b3acf1a78db781faee056451a43d912d60419
Kamil Kanclerz <kanclerz3@tlen.pl>
2018-10-14 11:14:11 +0200

    Added response Http Headers to GET request
    Added response Http Headers to GET request

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -22,9 +22,14 @@ export class DataStorageService {
   getRecipes() {
     const token = this.authService.getToken();
 
-    this.httpClient.get<Recipe[]>('https://ng-recipe-book-86501.firebaseio.com/recipes.json?auth=' + token)
+    // this.httpClient.get<Recipe[]>('https://ng-recipe-book-86501.firebaseio.com/recipes.json?auth=' + token)
+    this.httpClient.get<Recipe[]>('https://ng-recipe-book-86501.firebaseio.com/recipes.json?auth=' + token, {
+      observe: 'body',
+      responseType: 'json'
+    })
       .map(
         (recipes) => {
+          console.log(recipes);
           for (let recipe of recipes) {
             if (!recipe['ingredients']) {
               recipe['ingredients'] = [];
